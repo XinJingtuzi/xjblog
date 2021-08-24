ac8e11d97a5a8dce6e7c6086f7e1431ece0218ff
xinjing <xin709717837@163.com>
2021-08-24 09:46:49 +0800

    upday3

--- src/templates/compInfo.vue
+++ src/templates/compInfo.vue
@@ -18,7 +18,7 @@
       </div>
       <div class="content">
         <p>
-          <g-image :src="`/uploads${$page.compData.icon.url}`" width="2560"/>
+          <g-image :src="`${$page.compData.icon.url}`" width="2560"/>
         </p>
       </div>
     </div>
